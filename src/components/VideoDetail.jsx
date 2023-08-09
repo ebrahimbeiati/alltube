79bf53c98cf88e75d6bd475f74829adb88d56456
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-08-09 23:11:07 +0100

    fix the height for description

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,3 +1,42 @@
+// import { Paper, Typography } from "@mui/material";
+// import React from "react";
+
+// const VideoDetail = ({ video }) => {
+//   const {
+//     id: { videoId },
+//     snippet: { title, channelTitle, description },
+//   } = video;
+
+//   if (!videoId) {
+//     return <div>Type in search box please</div>;
+//   }
+
+//   const videoSrc = `https://www.youtube.com/embed/${videoId}`;
+
+//   return (
+//     <>
+//       <Paper elevation={7}  style={{ height:"70%" }}>
+//         <iframe
+//           src={videoSrc}
+//           frameBorder="0"
+//           height="70%"
+//           width="100%"
+//           title="video player"
+//         />
+//       </Paper>
+//       <Paper elevation={6} style={{ padding:"20px" }}>
+//         <Typography variant="h4">
+//           {title}-{channelTitle}
+//         </Typography>
+//         <Typography variant="body1">{channelTitle}</Typography>
+//         <Typography variant="body2">{description}</Typography>
+//       </Paper>
+//     </>
+//   );
+// };
+
+// export default VideoDetail;
+
 import { Paper, Typography } from "@mui/material";
 import React from "react";
 
@@ -8,28 +47,28 @@ const VideoDetail = ({ video }) => {
   } = video;
 
   if (!videoId) {
-    return <div>Loading...</div>;
+    return <div>Type in search box please</div>;
   }
 
   const videoSrc = `https://www.youtube.com/embed/${videoId}`;
 
   return (
     <>
-      <Paper elevation={3} sx={{ p: 2 }} style={{ height: "70%" }}>
+      <Paper elevation={6} style={{ height: "50%" }}>
         <iframe
-          src={videoSrc}
           frameBorder="0"
           height="100%"
           width="100%"
-          title="video player"
+          title="Video Player"
+          src={videoSrc}
         />
       </Paper>
-      <Paper elevation={3} sx={{ p: 2 }}>
-        <Typography variant="h6" component="div">
-          {title}
+      <Paper elevation={6} style={{ padding: "15px" }}>
+        <Typography variant="h4">
+          {title} - {channelTitle}
         </Typography>
-        <Typography variant="body2">{channelTitle}</Typography>
-        <Typography variant="body2">{description}</Typography>
+        <Typography variant="subtitle1">{channelTitle}</Typography>
+        <Typography variant="subtitle2">{description}</Typography>
       </Paper>
     </>
   );
