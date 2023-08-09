822b9be9fe0ef12e84930882849ececc12942e5d
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-08-09 23:11:42 +0100

    fix the search bar functionality problem

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,32 @@
 import { useState } from "react";
-import {Paper,TextField} from "@mui/material"
+import { Paper, TextField } from "@mui/material";
 
-
-
-const SearchBar= ({onSubmit})=> {
-    const [searchTerm, setSearchTerm] = useState("");
-    const handleChange = (e) => {
-        setSearchTerm(e.target.value);
-
+const SearchBar = ({ onSubmit }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onSubmit(searchTerm);
     }
-    const onKeyPress = (e) => {
-        if(e.key === "Enter"){
-            onSubmit(searchTerm)
-        }
-    }
+  };
 
-    return (
-        <Paper elevation={3} sx={{p:2, display:"flex", justifyContent:"center"}}>
-            <TextField label="Search" variant="outlined" fullWidth  value={searchTerm} onChange={handleChange} onKeyPress={onKeyPress} />
-        </Paper>
-    )
-    
-
-}
-
-
+  return (
+    <Paper
+      elevation={3}
+      sx={{ p: 2, display: "flex", justifyContent: "center" }}
+    >
+      <TextField
+        label="Search"
+        variant="outlined"
+        fullWidth
+        value={searchTerm}
+        onChange={handleChange}
+        onKeyPress={onKeyPress}
+      />
+    </Paper>
+  );
+};
 
 export default SearchBar;
-
