36baf6da60a36385a201cbf4da056ac16fc57b41
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-11-20 11:52:01 +0000

    migrating from react to vite

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,6 +1,75 @@
-import { Paper, Typography } from "@mui/material";
+// import { Paper, Typography } from "@mui/material";
+// import useMediaQuery from "@mui/material/useMediaQuery";
+
+// const VideoDetail = ({ video }) => {
+//   if (!video) {
+//     return <div>Loading...</div>; // Or any placeholder or loading indicator
+//   }
+
+//   const {
+//     id: { videoId },
+//     snippet: { title, channelTitle, description },
+//   } = video;
+
+//   if (!videoId) {
+//     return (
+//       <div
+//         style={{
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//           height: "20vh",
+//           width: "100%",
+//           fontWeight: "bold",
+//           marginLeft: "30%",
+//           fontSize: "30px",
+//           borderRadius: "15px",
+//           backgroundColor: "lightGray",
+//         }}
+//       >
+//         Enjoy exploring amazing videos
+//       </div>
+//     );
+//   }
+
+//   const videoSrc = `https://www.youtube.com/embed/${videoId}`;
+
+//   return (
+//     <>
+//       <Paper elevation={7} style={{ width: "100%", height: "20%" }}>
+//         <iframe
+//           src={videoSrc}
+//           frameBorder="0"
+//           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//           allowFullScreen
+//           style={{ width: "100%", height: "100%" }}
+//           title="video player"
+//         />
+//       </Paper>
+//       <div
+//         style={{
+//           padding: "3px",
+//         }}
+//       >
+//         <Paper elevation={6} style={{ padding: "10px" }}>
+//           <Typography variant="h5">
+//             {title}-{channelTitle}
+//           </Typography>
+//           <Typography variant="body1">{channelTitle}</Typography>
+//           <Typography variant="body2">{description}</Typography>
+//         </Paper>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default VideoDetail;
+
+import { Paper, Typography, useMediaQuery } from "@mui/material";
 
 const VideoDetail = ({ video }) => {
+  const isSmallScreen = useMediaQuery("(max-width: 600px)");
+
   if (!video) {
     return <div>Loading...</div>; // Or any placeholder or loading indicator
   }
@@ -17,11 +86,11 @@ const VideoDetail = ({ video }) => {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          height: "20vh",
+          height: "30vh",
           width: "100%",
           fontWeight: "bold",
           marginLeft: "30%",
-          fontSize: "30px",
+          fontSize: "20px",
           borderRadius: "15px",
           backgroundColor: "lightGray",
         }}
@@ -35,13 +104,16 @@ const VideoDetail = ({ video }) => {
 
   return (
     <>
-      <Paper elevation={7} style={{ width: "100%", height: "20%" }}>
+      <Paper
+        elevation={12}
+        style={{ width: "100%", height: isSmallScreen ? "40%" : "30%" }}
+      >
         <iframe
           src={videoSrc}
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          style={{ width: "100%", height: "100%" }}
+         
+          style={{ width: "100%", height: "50%" }}
           title="video player"
         />
       </Paper>
@@ -50,7 +122,7 @@ const VideoDetail = ({ video }) => {
           padding: "3px",
         }}
       >
-        <Paper elevation={6} style={{ padding: "10px" }}>
+        <Paper elevation={7} style={{ padding: "10px" }}>
           <Typography variant="h5">
             {title}-{channelTitle}
           </Typography>
@@ -63,5 +135,3 @@ const VideoDetail = ({ video }) => {
 };
 
 export default VideoDetail;
-
-
--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -285,39 +285,167 @@
 
 // export default VideoItem;
 
-import React from "react";
-import { Paper, Typography, Grid } from "@mui/material";
-import useMediaQuery from "@mui/material/useMediaQuery";
+// import React from "react";
+// import { Paper, Typography, Grid } from "@mui/material";
+// import useMediaQuery from "@mui/material/useMediaQuery";
+
+// const VideoItem = ({ video, onSelectVideo }) => {
+//   const isSmallScreen = useMediaQuery("(max-width: 600px)");
+
+//   return (
+//     <Grid container spacing={isSmallScreen ? 3 : 0}>
+//       <Grid item xs={12}>
+//         <Paper
+//           elevation={6}
+//           style={{
+         
+//             height:"auto",
+//             marginTop: "5px",
+//             borderRadius: "10px",
+//             cursor: "pointer",
+//             "&:hover": {
+//               transform: "scale(1.03)",
+//               boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
+//             },
+//           }}
+     
+//           onClick={() => onSelectVideo(video)}
+//         >
+//           <img
+//             style={{
+//               width: isSmallScreen ? "100%" : "100%",
+//               borderRadius: "8px",
+//             }}
+//             alt="thumbnail"
+//             src={video.snippet.thumbnails.medium.url}
+//           />
+//           <Typography
+//             variant="subtitle1"
+//             style={{
+//               fontWeight: "bold",
+//               padding: "10px",
+//               width: isSmallScreen ? "100%" : "60%",
+//             }}
+//           >
+//             {video.snippet.title}
+//           </Typography>
+//         </Paper>
+//       </Grid>
+//       {!isSmallScreen && (
+//         <Grid item xs={12}>
+//           <Paper
+//             elevation={6}
+//             style={{
+//               display: "fixed",
+//               alignItems: "center",
+//               width: "100%",
+//               marginBottom: "10px",
+//               borderRadius: "10px",
+//             }}
+//             onClick={() => onSelectVideo(video)}
+//           >
+//           </Paper>
+       
+        
+//   <Grid item xs={12}>
+//     <Paper>xs=8</Paper>
+//   </Grid>
+//   <Grid item xs={4}>
+//     <Paper>xs=4</Paper>
+//   </Grid>
+//   <Grid item xs={4}>
+//     <Paper>xs=4</Paper>
+//   </Grid>
+//   <Grid item xs={4}>
+//     <Paper>xs=4</Paper>
+//   </Grid>
+//         </Grid>
+
+//       )}
+//     </Grid>
+//   );
+// };
+
+// export default VideoItem;
+// import { Grid, Paper, Typography } from "@mui/material";
+// import useMediaQuery from "@mui/material/useMediaQuery";
+
+// const VideoItem = ({ video, onSelectVideo }) => {
+//   const isSmallScreen = useMediaQuery("(max-width: 600px)");
+
+//   return (
+//     <Grid container spacing={isSmallScreen ? 3 : 0}>
+//       <Grid item xs={12}>
+//         <Paper
+//           elevation={6}
+//           style={{
+//             height: "auto",
+//             marginTop: "5px",
+//             borderRadius: "10px",
+//             cursor: "pointer",
+//             "&:hover": {
+//               transform: "scale(1.03)",
+//               boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
+//             },
+//           }}
+//           onClick={() => onSelectVideo(video)}
+//         >
+//           <img
+//             style={{
+//               width: "100%",
+//               height: "auto",
+//               borderRadius: "8px",
+//             }}
+//             alt="thumbnail"
+//             src={video.snippet.thumbnails.medium.url}
+//           />
+//           <Typography
+//             variant="subtitle1"
+//             style={{
+//               fontWeight: "bold",
+//               padding: "10px",
+//               width: "100%",
+//             }}
+//           >
+//             {video.snippet.title}
+//           </Typography>
+//         </Paper>
+//       </Grid>
+//       {/* Add rest of the Grid items as needed */}
+//     </Grid>
+//   );
+// };
+
+// export default VideoItem;
+import { Grid, Paper, Typography, useMediaQuery } from "@mui/material";
 
 const VideoItem = ({ video, onSelectVideo }) => {
   const isSmallScreen = useMediaQuery("(max-width: 600px)");
 
   return (
     <Grid container spacing={isSmallScreen ? 2 : 0}>
-      <Grid item xs={12}>
+      <Grid item xs={14}>
         <Paper
           elevation={6}
           style={{
-            
-            flexDirection: isSmallScreen ? "column" : "row",
-            alignItems: "center",
-            width: "100%",
-            height:"50vh",
-            marginTop: "5px",
+            height: isSmallScreen ? "auto" : "200px",
+            marginTop: "8px",
             borderRadius: "10px",
             cursor: "pointer",
             "&:hover": {
-              transform: "scale(1.03)",
+             
               boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
             },
           }}
-     
           onClick={() => onSelectVideo(video)}
         >
           <img
             style={{
-              width: isSmallScreen ? "100%" : "100%",
+              width: "100%",
+              height: isSmallScreen ? "auto" : "100px",
               borderRadius: "8px",
+              objectFit: "cover",
+              display:"flex",
             }}
             alt="thumbnail"
             src={video.snippet.thumbnails.medium.url}
@@ -326,31 +454,17 @@ const VideoItem = ({ video, onSelectVideo }) => {
             variant="subtitle1"
             style={{
               fontWeight: "bold",
-              padding: "10px",
-              width: isSmallScreen ? "100%" : "60%",
+              padding: "20px",
+              textAlign: "center",
+              width: "100%",
+              
             }}
           >
             {video.snippet.title}
           </Typography>
         </Paper>
       </Grid>
-      {!isSmallScreen && (
-        <Grid item xs={12}>
-          <Paper
-            elevation={6}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              width: "100%",
-              marginBottom: "10px",
-              borderRadius: "10px",
-            }}
-            onClick={() => onSelectVideo(video)}
-          >
-           
-          </Paper>
-        </Grid>
-      )}
+      {/* Add rest of the Grid items as needed */}
     </Grid>
   );
 };
